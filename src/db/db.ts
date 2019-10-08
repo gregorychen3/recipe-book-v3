ca4eb586b0580ed2653b8a3fac17e827e3ed1e69
Gregory Chen <gregorychen3@gmail.com>
2019-10-07 23:51:55 -0400

    export db initialization func

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,11 +4,13 @@ import l from "../logger";
 
 const MONGO_CONN_OPTS = { useNewUrlParser: true };
 
-l.info(`Connecting to mongo at ${MONGODB_URI}`);
-mongoose.connect(MONGODB_URI, MONGO_CONN_OPTS, err => {
-  if (err) {
-    l.error("Failed to connect to mongo");
-    process.exit(1);
-  }
-  l.info("Successfully connected to mongo");
-});
+export const initConnection = () => {
+  l.info(`Connecting to mongo at ${MONGODB_URI}`);
+  mongoose.connect(MONGODB_URI, MONGO_CONN_OPTS, err => {
+    if (err) {
+      l.error("Failed to connect to mongo");
+      process.exit(1);
+    }
+    l.info("Successfully connected to mongo");
+  });
+};
