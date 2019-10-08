8340c0a8fadbb18cd45f6ecada31917f09998a4c
Gregory Chen <gregorychen3@gmail.com>
2019-10-07 22:11:21 -0400

    convert to ts

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,33 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+import express, { NextFunction, Request, Response } from "express";
+import createError, { HttpError } from "http-errors";
+import logger from "morgan";
+import path from "path";
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
 
 var app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.sendStatus(err.status || 500);
+  res.sendStatus(err.status ? err.status : 500);
 });
 
 module.exports = app;
