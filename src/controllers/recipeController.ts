79f24b196747062e1f75caaa7d00c141d05dcecf
Gregory Chen <gchen213@bloomberg.net>
2019-10-08 12:18:01 -0400

    validate recipe name field

--- src/controllers/recipeController.ts
+++ src/controllers/recipeController.ts
@@ -1,9 +1,21 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { check, validationResult } from "express-validator/check";
+
+const recipeValidation = [check("name").exists(), check("name").isString()];
 
 const recipeController = express.Router();
 
 recipeController.get("/", (req, res, next) => {
   res.send("respond with a resource");
+});
+
+recipeController.post("/", recipeValidation, (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  return res.send("ok");
 });
 
 export default recipeController;
