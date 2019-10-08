05718876d9a3ed9ff8a2c56b4c1d5187ef37d1e1
Gregory Chen <gregorychen3@gmail.com>
2019-10-07 23:50:44 -0400

    add db.ts which initializes the database

---
+++ src/db/db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { MONGODB_URI } from "../config";
+import l from "../logger";
+
+const MONGO_CONN_OPTS = { useNewUrlParser: true };
+
+l.info(`Connecting to mongo at ${MONGODB_URI}`);
+mongoose.connect(MONGODB_URI, MONGO_CONN_OPTS, err => {
+  if (err) {
+    l.error("Failed to connect to mongo");
+    process.exit(1);
+  }
+  l.info("Successfully connected to mongo");
+});
