b0ce6b5bd2837c5b1e98a6a8cd45e4ff000073d4
Gregory Chen <gregorychen3@gmail.com>
2019-10-08 00:57:30 -0400

    skel code for recipeService api

--- src/services/recipeService.ts
+++ src/services/recipeService.ts
@@ -1,1 +1,6 @@
-import mongoose from "mongoose";
+const create = () => {};
+const list = () => {};
+const update = () => {};
+const remove = () => {};
+
+export default { create, list, update, remove };
