ede8d4be28abb9c447ceb1d4a3fa500f56c44ddb
Gregory Chen <gregorychen3@gmail.com>
2019-10-08 22:23:39 -0400

    wire up app with react redux provider

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunkMiddleware from "redux-thunk";
 import App from "./App";
 import "./index.css";
+import rootReducer from "./redux/reducers/index";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
