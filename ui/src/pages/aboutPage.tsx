c78ad693b730f46fe1208f6d4da55332e5d79f9a
Gregory Chen <gregorychen3@gmail.com>
2019-10-08 22:32:07 -0400

    page component skel

---
+++ ui/src/pages/aboutPage.tsx
@@ -0,0 +1,7 @@
+import { Component } from "react";
+
+export default class AboutPage extends Component {
+  render() {
+    return "AboutPage";
+  }
+}
---
+++ ui/src/pages/recipePage.tsx
@@ -0,0 +1,7 @@
+import { Component } from "react";
+
+export default class RecipePage extends Component {
+  render() {
+    return "RecipePage";
+  }
+}
