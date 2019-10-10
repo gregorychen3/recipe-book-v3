701477a536ce81a64187f5323d4c5d08a40b8867
Gregory Chen <gregorychen3@gmail.com>
2019-10-10 19:56:15 -0400

    make recipes page a functional component

--- ui/src/pages/recipesPage.tsx
+++ ui/src/pages/recipesPage.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators, Dispatch } from "redux";
 import { IRecipeModel } from "../../../src/db/recipe";
@@ -10,28 +10,27 @@ interface Props {
   recipes: IRecipeModel[];
   fetchRecipes: typeof fetchRecipes;
 }
-class RecipesPage extends Component<Props> {
-  componentDidMount() {
-    this.props.fetchRecipes();
-  }
+const RecipesPage = ({ recipes, fetchRecipes }: Props) => {
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
 
-  render() {
-    return (
-      <section className="section">
-        <div className="container">
-          <div className="is-divider" data-content="ANTIPASTI"></div>
-          <div className="is-divider" data-content="PRIMI"></div>
-          <div className="is-divider" data-content="SECONDI"></div>
-          <div className="is-divider" data-content="DOLCI"></div>
-          <div className="is-divider" data-content="CONTORNI"></div>
-          <div className="is-divider" data-content="SAUCES"></div>
-          <div className="is-divider" data-content="BEVERAGES"></div>
-          <div className="is-divider" data-content="OTHER"></div>
-        </div>
-      </section>
-    );
-  }
-}
+  return (
+    <section className="section">
+      <div className="container">
+        <div className="is-divider" data-content="ANTIPASTI"></div>
+        <div className="is-divider" data-content="PRIMI"></div>
+        <div className="is-divider" data-content="SECONDI"></div>
+        <div className="is-divider" data-content="DOLCI"></div>
+        <div className="is-divider" data-content="CONTORNI"></div>
+        <div className="is-divider" data-content="SAUCES"></div>
+        <div className="is-divider" data-content="BEVERAGES"></div>
+        <div className="is-divider" data-content="OTHER"></div>
+      </div>
+      {JSON.stringify(recipes)}
+    </section>
+  );
+};
 
 const mapStateToProps = (state: RootState) => ({ recipes: recipes(state) });
 const mapDispatchToProps = (dispatch: Dispatch<ActionTypes>) =>
