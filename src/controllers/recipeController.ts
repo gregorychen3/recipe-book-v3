dad484afbecc6fcf36da6b8ef8bd11cc9d6abeb3
Gregory Chen <gregorychen3@gmail.com>
2019-10-12 12:22:02 -0400

    fix bug where returning 404 for recipes that exist

--- src/controllers/recipeController.ts
+++ src/controllers/recipeController.ts
@@ -62,7 +62,7 @@ recipeController.get("/", async (req, res, next) => {
 
 recipeController.get("/:id", async (req, res, next) => {
   const recipe = await Recipe.findOne({ _id: req.params.id });
-  return recipe ? res.sendStatus(404) : res.send(recipe);
+  return recipe ? res.send(recipe) : res.sendStatus(404);
 });
 
 recipeController.post(
