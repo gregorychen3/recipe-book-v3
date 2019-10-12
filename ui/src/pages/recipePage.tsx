a829d1065146da887144ae854c8742e866e2a4b6
Gregory Chen <gregorychen3@gmail.com>
2019-10-12 12:01:26 -0400

    use url params in recipe page

--- ui/src/pages/recipePage.tsx
+++ ui/src/pages/recipePage.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
+import { useParams } from "react-router";
 import { bindActionCreators, Dispatch } from "redux";
+import { IRecipeModel } from "../../../src/db/recipe";
 import { ActionTypes, fetchRecipes } from "../redux/actions";
 import { RootState } from "../redux/reducers";
 import { recipes } from "../redux/selectors";
 
-const RecipePage = () => <div>RecipePage</div>;
+interface Props {
+  recipes: IRecipeModel[];
+  fetchRecipes: typeof fetchRecipes;
+}
+const RecipePage = ({ recipes, fetchRecipes }: Props) => {
+  let { recipeId } = useParams();
+
+  const recipe = recipes.find(r => r._id === recipeId);
+  if (!recipe) {
+    return null;
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">{recipe.name}</h1>
+    </div>
+  );
+};
 
 const mapStateToProps = (state: RootState) => ({ recipes: recipes(state) });
 const mapDispatchToProps = (dispatch: Dispatch<ActionTypes>) =>
