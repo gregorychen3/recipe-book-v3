0f2b9c257c85908987f851df545fa84be0fb07e6
Gregory Chen <gregorychen3@gmail.com>
2019-10-13 03:05:52 -0400

    wrap about page in section

--- ui/src/pages/aboutPage.tsx
+++ ui/src/pages/aboutPage.tsx
@@ -3,61 +3,63 @@ import React from "react";
 const imagePath = "./gregandally.jpg";
 
 const AboutPage = () => (
-  <div className="container">
-    <img src={imagePath} alt="gregandally" />
-    <h1 className="title is-spaced">Greg and Ally's Recipe Book</h1>
-    <h2 className="subtitle">Project Goals</h2>
-    <div className="content">
-      <ul>
-        <li>assist in everyday cooking</li>
-        <li>document research into traditional dishes</li>
-        <li>learn web-development technologies</li>
-      </ul>
+  <section className="section">
+    <div className="container">
+      <img src={imagePath} alt="gregandally" />
+      <h1 className="title is-spaced">Greg and Ally's Recipe Book</h1>
+      <h2 className="subtitle">Project Goals</h2>
+      <div className="content">
+        <ul>
+          <li>assist in everyday cooking</li>
+          <li>document research into traditional dishes</li>
+          <li>learn web-development technologies</li>
+        </ul>
+      </div>
+      <a href="https://github.com/gregorychen3/recipe-book">
+        <span className="subtitle">Tech Stack</span>
+        <span className="icon">
+          <i className="fab fa-github" />
+        </span>
+        GitHub
+      </a>
+      <div className="content">
+        <ul>
+          <li>
+            v1
+            <ul>
+              <li>Jinja templates</li>
+              <li>jQuery</li>
+              <li>Python Flask API</li>
+              <li>PostgreSQL</li>
+              <li>Deployed to EC2</li>
+            </ul>
+          </li>
+          <li>
+            v2
+            <ul>
+              <li>React UI</li>
+              <li>Express.js REST API</li>
+              <li>PostgreSQL</li>
+              <li>Deployed to EC2 via Docker</li>
+            </ul>
+          </li>
+          <li>
+            v3
+            <ul>
+              <li>React Hooks + Redux UI + Typescript</li>
+              <li>Express.js REST API + Typescript</li>
+              <li>MongoDB</li>
+              <li>Deployed to Heroku</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <h2 className="subtitle">Contact</h2>
+      Kindly direct all inquiries to
+      <a href="mailto:cenaacasaperdue@gmail.com">cenaacasaperdue@gmail.com</a>,
+      thank you.
     </div>
-    <a href="https://github.com/gregorychen3/recipe-book">
-      <span className="subtitle">Tech Stack</span>
-      <span className="icon">
-        <i className="fab fa-github" />
-      </span>
-      GitHub
-    </a>
-    <div className="content">
-      <ul>
-        <li>
-          v1
-          <ul>
-            <li>Jinja templates</li>
-            <li>jQuery</li>
-            <li>Python Flask API</li>
-            <li>PostgreSQL</li>
-            <li>Deployed to EC2</li>
-          </ul>
-        </li>
-        <li>
-          v2
-          <ul>
-            <li>React UI</li>
-            <li>Express.js REST API</li>
-            <li>PostgreSQL</li>
-            <li>Deployed to EC2 via Docker</li>
-          </ul>
-        </li>
-        <li>
-          v3
-          <ul>
-            <li>React Hooks + Redux UI + Typescript</li>
-            <li>Express.js REST API + Typescript</li>
-            <li>MongoDB</li>
-            <li>Deployed to Heroku</li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-    <h2 className="subtitle">Contact</h2>
-    Kindly direct all inquiries to
-    <a href="mailto:cenaacasaperdue@gmail.com">cenaacasaperdue@gmail.com</a>,
-    thank you.
-  </div>
+  </section>
 );
 
 export default AboutPage;
