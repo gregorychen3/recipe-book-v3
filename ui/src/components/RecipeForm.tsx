2bd092006193cfa00b33b3c6310eecf850a28312
Gregory Chen <gregorychen3@gmail.com>
2019-10-13 03:18:59 -0400

    wrap recipe form in <Form>

--- ui/src/components/RecipeForm.tsx
+++ ui/src/components/RecipeForm.tsx
@@ -1,4 +1,4 @@
-import { FormikProps, withFormik } from "formik";
+import { Form, FormikProps, withFormik } from "formik";
 import React from "react";
 import { useHistory } from "react-router";
 import { IRecipeModel } from "../../../src/db/recipe";
@@ -31,151 +31,153 @@ const InnerForm = (props: OtherProps & FormikProps<FormValues>) => {
   const { touched, errors, isSubmitting } = props;
   let history = useHistory();
   return (
-    <section className="section">
-      <div className="container">
-        <h1 className="title has-text-centered">
-          <a className="button is-white is-pulled-left is-invisible">
-            <span className="icon has-text-info is-small">
-              <i className="fas fa-edit" />
-            </span>
-          </a>
-          {props.values.name}
-          <a
-            onClick={() => history.push(`/recipes/${props.recipe._id}`)}
-            className="button is-white is-pulled-right"
-          >
-            <span className="icon has-text-info is-small">
-              <i className="fas fa-save" />
-            </span>
-          </a>
-        </h1>
+    <Form>
+      <section className="section">
+        <div className="container">
+          <h1 className="title has-text-centered">
+            <a className="button is-white is-pulled-left is-invisible">
+              <span className="icon has-text-info is-small">
+                <i className="fas fa-edit" />
+              </span>
+            </a>
+            {props.values.name}
+            <a
+              onClick={() => history.push(`/recipes/${props.recipe._id}`)}
+              className="button is-white is-pulled-right"
+            >
+              <span className="icon has-text-info is-small">
+                <i className="fas fa-save" />
+              </span>
+            </a>
+          </h1>
 
-        <nav className="level">
-          <p className="level-item has-text-centered" />
-          <div className="level-item has-text-centered">
-            <div className="field">
-              <div className="control has-icons-left">
-                <div className="select">
-                  <select>
-                    {ICourseValues.map(c => (
-                      <option value={c} label={capitalize(c)} key={c} />
-                    ))}
-                  </select>
-                </div>
-                <div className="icon is-small is-left">
-                  <i className="fas fa-utensils"></i>
+          <nav className="level">
+            <p className="level-item has-text-centered" />
+            <div className="level-item has-text-centered">
+              <div className="field">
+                <div className="control has-icons-left">
+                  <div className="select">
+                    <select>
+                      {ICourseValues.map(c => (
+                        <option value={c} label={capitalize(c)} key={c} />
+                      ))}
+                    </select>
+                  </div>
+                  <div className="icon is-small is-left">
+                    <i className="fas fa-utensils"></i>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="level-item has-text-centered">
-            <div className="field">
-              <div className="control has-icons-left">
-                <div className="select">
-                  <select>
-                    {ICuisineValues.map(c => (
-                      <option value={c} label={capitalize(c)} key={c} />
-                    ))}
-                  </select>
-                </div>
-                <div className="icon is-small is-left">
-                  <i className="fas fa-globe"></i>
+            <div className="level-item has-text-centered">
+              <div className="field">
+                <div className="control has-icons-left">
+                  <div className="select">
+                    <select>
+                      {ICuisineValues.map(c => (
+                        <option value={c} label={capitalize(c)} key={c} />
+                      ))}
+                    </select>
+                  </div>
+                  <div className="icon is-small is-left">
+                    <i className="fas fa-globe"></i>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <p className="level-item has-text-centered" />
-        </nav>
+            <p className="level-item has-text-centered" />
+          </nav>
 
-        {props.recipe.ingredients.length > 0 && (
-          <>
-            <div className="is-divider" data-content="INGREDIENTS" />
-            <div className="content">
-              <ul>
-                {props.recipe.ingredients.map((ingredient, idx) => (
-                  <li key={idx}>
-                    <div className="field is-horizontal">
-                      <div className="field-body">
-                        <div className="field">
-                          <p className="control is-expanded">
-                            <input
-                              className="input"
-                              type="number"
-                              placeholder="Quantity"
-                              value={ingredient.qty}
-                            />
-                          </p>
-                        </div>
-                        <div className="field">
-                          <p className="control is-expanded">
-                            <input
-                              className="input"
-                              type="text"
-                              placeholder="Unit"
-                              value={ingredient.unit}
-                            />
-                          </p>
-                        </div>
-                        <div className="field">
-                          <p className="control is-expanded">
-                            <input
-                              className="input"
-                              type="text"
-                              placeholder="Name"
-                              value={ingredient.name}
-                            />
-                          </p>
+          {props.recipe.ingredients.length > 0 && (
+            <>
+              <div className="is-divider" data-content="INGREDIENTS" />
+              <div className="content">
+                <ul>
+                  {props.recipe.ingredients.map((ingredient, idx) => (
+                    <li key={idx}>
+                      <div className="field is-horizontal">
+                        <div className="field-body">
+                          <div className="field">
+                            <p className="control is-expanded">
+                              <input
+                                className="input"
+                                type="number"
+                                placeholder="Quantity"
+                                value={ingredient.qty}
+                              />
+                            </p>
+                          </div>
+                          <div className="field">
+                            <p className="control is-expanded">
+                              <input
+                                className="input"
+                                type="text"
+                                placeholder="Unit"
+                                value={ingredient.unit}
+                              />
+                            </p>
+                          </div>
+                          <div className="field">
+                            <p className="control is-expanded">
+                              <input
+                                className="input"
+                                type="text"
+                                placeholder="Name"
+                                value={ingredient.name}
+                              />
+                            </p>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </>
-        )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </>
+          )}
 
-        {props.recipe.instructions.length > 0 && (
-          <>
-            <div className="is-divider" data-content="INSTRUCTIONS" />
-            <div className="content">
-              <ol type="1">
-                {props.recipe.instructions.map((instruction, idx) => (
-                  <li key={idx}>
-                    <input
-                      className="input"
-                      type="text"
-                      value={instruction}
-                      placeholder="Each step on its own line"
-                    />
-                  </li>
-                ))}
-              </ol>
-            </div>
-          </>
-        )}
+          {props.recipe.instructions.length > 0 && (
+            <>
+              <div className="is-divider" data-content="INSTRUCTIONS" />
+              <div className="content">
+                <ol type="1">
+                  {props.recipe.instructions.map((instruction, idx) => (
+                    <li key={idx}>
+                      <input
+                        className="input"
+                        type="text"
+                        value={instruction}
+                        placeholder="Each step on its own line"
+                      />
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </>
+          )}
 
-        {props.recipe.sources.length > 0 && (
-          <>
-            <div className="is-divider" data-content="SOURCES" />
-            <div className="content">
-              <ul>
-                {props.recipe.sources.map(s => (
-                  <li key={s}>
-                    <input
-                      className="input"
-                      type="text"
-                      value={s}
-                      placeholder="Enter source"
-                    />
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </>
-        )}
-      </div>
-    </section>
+          {props.recipe.sources.length > 0 && (
+            <>
+              <div className="is-divider" data-content="SOURCES" />
+              <div className="content">
+                <ul>
+                  {props.recipe.sources.map(s => (
+                    <li key={s}>
+                      <input
+                        className="input"
+                        type="text"
+                        value={s}
+                        placeholder="Enter source"
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </>
+          )}
+        </div>
+      </section>
+    </Form>
   );
 
   /*
