d38c330550c9b67b5a32552ca74a12df98a84ccd
Gregory Chen <gregorychen3@gmail.com>
2019-10-14 22:04:43 -0400

    convert navbar to functional component

--- ui/src/components/Navbar.tsx
+++ ui/src/components/Navbar.tsx
@@ -1,67 +1,62 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-export default class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <Link to={"/recipes"} className="navbar-item">
-            <span className="icon">
-              <i className="fas fa-utensils" />
-            </span>
-            <span>
-              <strong>Greg and Ally's Recipe Book</strong>
-            </span>
-          </Link>
+const Navbar = () => (
+  <nav className="navbar" role="navigation" aria-label="main navigation">
+    <div className="navbar-brand">
+      <Link to={"/recipes"} className="navbar-item">
+        <span className="icon">
+          <i className="fas fa-utensils" />
+        </span>
+        <span>
+          <strong>Greg and Ally's Recipe Book</strong>
+        </span>
+      </Link>
 
-          <a
-            role="button"
-            className="navbar-burger burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-            href="#/"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
+      <a
+        role="button"
+        className="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+        href="#/"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" className="navbar-menu">
+      <div className="navbar-start">
+        <div className="navbar-item has-dropdown is-hoverable">
+          <a className="navbar-link" href="#/">
+            Browse
           </a>
-        </div>
-
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start">
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link" href="#/">
-                Browse
-              </a>
-              <div className="navbar-dropdown">
-                <Link to={"/recipes?groupBy=course"} className="navbar-item">
-                  By Course
-                </Link>
-                <Link to={"/recipes?groupBy=cuisine"} className="navbar-item">
-                  By Cuisine
-                </Link>
-                <Link
-                  to={"/recipes?groupBy=alphabetical"}
-                  className="navbar-item"
-                >
-                  A-Z
-                </Link>
-              </div>
-            </div>
-            <a className="navbar-item" href="#/">
-              Add Recipe
-            </a>
-          </div>
-
-          <div className="navbar-end">
-            <Link to={"/about"} className="navbar-item">
-              About
+          <div className="navbar-dropdown">
+            <Link to={"/recipes?groupBy=course"} className="navbar-item">
+              By Course
+            </Link>
+            <Link to={"/recipes?groupBy=cuisine"} className="navbar-item">
+              By Cuisine
+            </Link>
+            <Link to={"/recipes?groupBy=alphabetical"} className="navbar-item">
+              A-Z
             </Link>
           </div>
         </div>
-      </nav>
-    );
-  }
-}
+        <a className="navbar-item" href="#/">
+          Add Recipe
+        </a>
+      </div>
+
+      <div className="navbar-end">
+        <Link to={"/about"} className="navbar-item">
+          About
+        </Link>
+      </div>
+    </div>
+  </nav>
+);
+
+export default Navbar;
