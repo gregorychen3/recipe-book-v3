752e3fc39ff1a403109e355bd78a68102ca97fcc
Gregory Chen <gregorychen3@gmail.com>
2019-10-15 20:54:33 -0400

    admin login modal skel

---
+++ ui/src/components/AdminLoginModal.tsx
@@ -0,0 +1,11 @@
+import React from "React";
+
+const AdminLoginModal = () => {
+  <div className="modal">
+    <div className="modal-background"></div>
+    <div className="modal-content">Admin login </div>
+    <button className="modal-close is-large" aria-label="close"></button>
+  </div>;
+};
+
+export default AdminLoginModal;
