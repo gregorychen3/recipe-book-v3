e17ed4354ecaa67c8f3f6b7fb19f4e83adad00ff
Gregory Chen <gregorychen3@gmail.com>
2019-10-15 21:06:05 -0400

    add modal visibility selector

--- ui/src/redux/selectors.ts
+++ ui/src/redux/selectors.ts
@@ -1,3 +1,6 @@
 import { RootState } from "./reducers/index";
 
 export const recipes = (state: RootState) => state.data.recipes;
+
+export const adminLoginModalVisibility = (state: RootState) =>
+  state.ui.adminLoginModalVisibility;
