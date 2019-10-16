248d28244c777ece407ed986e4964d1938ad8791
Gregory Chen <gregorychen3@gmail.com>
2019-10-15 23:49:07 -0400

    take ADMIN_PASSWORD env var

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,14 @@
+const exitWithError = (msg: string) => {
+  console.error(`${msg} environment variable not set. Exiting.`);
+  process.exit(1);
+};
+
 export const PORT = process.env.PORT || 3000;
 
 export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/recipebook";
+
+if (!process.env.ADMIN_PASSWORD) {
+  exitWithError("ADMIN_PASSWORD");
+}
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
