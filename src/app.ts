e71ae259d0d96d448c325b063d42c4376c104f5f
Gregory Chen <gregorychen3@gmail.com>
2019-10-15 23:23:01 -0400

    serve react app from express

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,9 @@ import { initDbConn } from "./db/db";
 
 const app = express();
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "/../ui/build")));
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,8 +18,16 @@ app.use(express.static(path.join(__dirname, "public")));
 
 initDbConn();
 
-app.use("/", testController);
-app.use("/recipes", recipeController);
+app.use("/api/", testController);
+app.use("/api/recipes", recipeController);
+
+// the "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  const file = path.join(__dirname + "/../ui/build/index.html");
+  console.log(__dirname);
+  res.sendFile(file);
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => next(createError(404)));
