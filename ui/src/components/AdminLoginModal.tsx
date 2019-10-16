3413763fbb5c3bc0837b08e59ff35ccaec496b16
Gregory Chen <gregorychen3@gmail.com>
2019-10-15 21:33:34 -0400

    use modal card

--- ui/src/components/AdminLoginModal.tsx
+++ ui/src/components/AdminLoginModal.tsx
@@ -11,13 +11,26 @@ interface Props {
 const AdminLoginModal = ({ onHide }: Props) => {
   return (
     <div className="modal is-active">
-      <div className="modal-background"></div>
-      <div className="modal-content">Admin login </div>
-      <button
-        className="modal-close is-large"
-        aria-label="close"
-        onClick={() => onHide()}
-      ></button>
+      <div className="modal-background" onClick={() => onHide()}></div>
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">Administrator Login</p>
+          <button
+            className="delete"
+            aria-label="close"
+            onClick={() => onHide()}
+          ></button>
+        </header>
+        <section className="modal-card-body">
+          <input className="input" type="password" placeholder="Password" />
+        </section>
+        <footer className="modal-card-foot">
+          <button className="button" onClick={() => onHide()}>
+            Cancel
+          </button>
+          <button className="button is-success">Submit</button>
+        </footer>
+      </div>
     </div>
   );
 };
