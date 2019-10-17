9959e42432d2cc60cc13b231e113e8df59461986
Gregory Chen <gregorychen3@gmail.com>
2019-10-16 20:15:43 -0400

    wire up burger menu

--- ui/src/components/Navbar.tsx
+++ ui/src/components/Navbar.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import classNames from "classnames";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { bindActionCreators, Dispatch } from "redux";
@@ -8,67 +9,77 @@ import { ActionTypes, createRecipe } from "../redux/actions";
 interface Props {
   createRecipe: typeof createRecipe;
 }
-const Navbar = ({ createRecipe }: Props) => (
-  <nav className="navbar" role="navigation" aria-label="main navigation">
-    <div className="navbar-brand">
-      <Link to={"/recipes"} className="navbar-item">
-        <span className="icon">
-          <i className="fas fa-utensils" />
-        </span>
-        <span>
-          <strong>Greg and Ally's Recipe Book</strong>
-        </span>
-      </Link>
+const Navbar = ({ createRecipe }: Props) => {
+  const [showBurgerMenu, setShowBurgerMenu] = useState(false);
 
-      <a
-        role="button"
-        className="navbar-burger burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
-        href="#/"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <Link to={"/recipes"} className="navbar-item">
+          <span className="icon">
+            <i className="fas fa-utensils" />
+          </span>
+          <span>
+            <strong>Greg and Ally's Recipe Book</strong>
+          </span>
+        </Link>
 
-    <div id="navbarBasicExample" className="navbar-menu">
-      <div className="navbar-start">
-        <div className="navbar-item has-dropdown is-hoverable">
-          <a className="navbar-link" href="#/">
-            Browse
-          </a>
-          <div className="navbar-dropdown">
-            <Link to={"/recipes?groupBy=course"} className="navbar-item">
-              By Course
-            </Link>
-            <Link to={"/recipes?groupBy=cuisine"} className="navbar-item">
-              By Cuisine
-            </Link>
-            <Link to={"/recipes?groupBy=alphabetical"} className="navbar-item">
-              A-Z
-            </Link>
-          </div>
-        </div>
         <a
-          className="navbar-item"
-          onClick={() => createRecipe(defaultRecipe())}
+          onClick={() => setShowBurgerMenu(!showBurgerMenu)}
+          className="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          role="button"
           href="#/"
         >
-          Add Recipe
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div className="navbar-end">
-        <Link to={"/about"} className="navbar-item">
-          About
-        </Link>
+      <div
+        id="navbarBasicExample"
+        className={classNames("navbar-menu", { "is-active": showBurgerMenu })}
+      >
+        <div className="navbar-start">
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a className="navbar-link" href="#/">
+              Browse
+            </a>
+            <div className="navbar-dropdown">
+              <Link to={"/recipes?groupBy=course"} className="navbar-item">
+                By Course
+              </Link>
+              <Link to={"/recipes?groupBy=cuisine"} className="navbar-item">
+                By Cuisine
+              </Link>
+              <Link
+                to={"/recipes?groupBy=alphabetical"}
+                className="navbar-item"
+              >
+                A-Z
+              </Link>
+            </div>
+          </div>
+          <a
+            className="navbar-item"
+            onClick={() => createRecipe(defaultRecipe())}
+            href="#/"
+          >
+            Add Recipe
+          </a>
+        </div>
+
+        <div className="navbar-end">
+          <Link to={"/about"} className="navbar-item">
+            About
+          </Link>
+        </div>
       </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 const mapDispatchToProps = (dispatch: Dispatch<ActionTypes>) =>
   bindActionCreators({ createRecipe }, dispatch);
