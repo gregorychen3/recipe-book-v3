102a85a44b38d031b1b6e709b08428cf01160d16
Gregory Chen <gchen213@bloomberg.net>
2019-11-19 11:29:29 -0500

    add isFetchingRecipes to ui reducer

--- ui/src/redux/reducers/ui.ts
+++ ui/src/redux/reducers/ui.ts
@@ -1,5 +1,8 @@
 import {
   ActionTypes,
+  FETCH_RECIPES_FAILURE,
+  FETCH_RECIPES_REQUEST,
+  FETCH_RECIPES_SUCCESS,
   HIDE_ADMIN_LOGIN_MODAL,
   SHOW_ADMIN_LOGIN_MODAL
 } from "../actions";
@@ -7,16 +10,26 @@ import {
 export type PriceSubmissionStatus = "succeeded" | "failed";
 
 export interface UIState {
+  isFetchingRecipes: boolean;
   adminLoginModalVisibility: boolean;
 }
 
-const initialState: UIState = { adminLoginModalVisibility: false };
+const initialState: UIState = {
+  isFetchingRecipes: false,
+  adminLoginModalVisibility: false
+};
 
 const uiReducer = (
   state: UIState = initialState,
   action: ActionTypes
 ): UIState => {
   switch (action.type) {
+    case FETCH_RECIPES_REQUEST:
+      return { ...state, isFetchingRecipes: true };
+    case FETCH_RECIPES_SUCCESS:
+      return { ...state, isFetchingRecipes: false };
+    case FETCH_RECIPES_FAILURE:
+      return { ...state, isFetchingRecipes: false };
     case SHOW_ADMIN_LOGIN_MODAL:
       return { ...state, adminLoginModalVisibility: true };
     case HIDE_ADMIN_LOGIN_MODAL:
